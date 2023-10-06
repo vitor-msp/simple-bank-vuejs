2f7c8242e24bf18caee0884d313fe3d51c6865de
Vitor Mateus <vitor7jan@gmail.com>
2023-10-05 23:37:00 -0300

    feat(vuejs): added signup form

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,52 @@
+<script lang="ts">
+import type { Customer } from '@/core/domain/Customer';
+import { createAccountUsecase } from '@/factory';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "SignupForm",
+    data() {
+        const customer: Customer = {
+            name: "",
+            cpf: "",
+        }
+        return { customer }
+    },
+    methods: {
+        async signUp() {
+            const account = true
+            // await createAccountUsecase.execute({
+            //     cpf: this.customer.cpf,
+            //     name: this.customer.name
+            // });
+            alert(JSON.stringify(this.customer));
+            if (!account) return;
+            alert(JSON.stringify(account));
+            this.$router.push("/login")
+        }
+    }
+})
+</script>
+
+<template>
+    <div>
+        <h2>sign up</h2>
+        <form @submit.prevent="signUp()">
+            <fieldset>
+                <div>
+                    <label htmlFor="name">name</label>
+                    <input type="text" id="name" v-model="customer.name" />
+                </div>
+                <div>
+                    <label htmlFor="text">cpf</label>
+                    <input type="text" id="cpf" v-model="customer.cpf" />
+                </div>
+            </fieldset>
+            <div>
+                <button type="submit">
+                    sign up
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,8 +1,15 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from "vue";
+import SignupForm from "../components/SignupForm.vue"
+
+export default defineComponent({
+  name: "SignupView",
+  components: { SignupForm }
+})
 </script>
 
 <template>
   <main>
-    <p>sign up</p>
+    <SignupForm />
   </main>
 </template>
