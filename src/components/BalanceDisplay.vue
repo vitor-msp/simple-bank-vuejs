883db4602c3578be0ce7bcac3cf94c723aeef697
Vitor Mateus <vitor7jan@gmail.com>
2023-10-11 23:22:58 -0300

    feat(vuejs): added balance display

--- src/components/BalanceDisplay.vue
+++ src/components/BalanceDisplay.vue
@@ -1,14 +1,33 @@
 <script lang="ts">
+import type { GetBalanceOutput } from '@/core/gateways/IHttpGateway';
+import { getBalanceUsecase } from '@/factory';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: "BalanceDisplay",
-    data() { return {} }
+    data() {
+        let balance: number = 0;
+        return { balance }
+    },
+    mounted() {
+        (async () => {
+            const account = true//await accountContext.getAccount();
+            if (!account) return;
+            const response: GetBalanceOutput = { balance: 15 }//await getBalanceUsecase.execute(account.accountNumber!);
+            if (response) this.balance = response.balance;
+        })();
+    }
 })
 </script>
 
 <template>
     <div>
-        <span>balance</span>
+        <span>Balance:</span>
+        <span>
+            {{ balance.toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+            }) }}
+        </span>
     </div>
 </template>
