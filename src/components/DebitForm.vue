ba60aca8d68d521e2ea1d122317238c92b670994
Vitor Mateus <vitor7jan@gmail.com>
2023-10-11 22:40:36 -0300

    feat(vuejs): added debit form

--- src/components/DebitForm.vue
+++ src/components/DebitForm.vue
@@ -1,6 +1,44 @@
 <script lang="ts">
+import { defineComponent } from 'vue';
+import type { Debit } from '@/core/domain/Debit';
+import { debitUsecase } from '@/factory';
+
+
+export default defineComponent({
+    name: "DebitForm",
+    data() {
+        const debit: Debit = {
+            createdAt: new Date(),
+            value: 0
+        }
+        return { debit }
+    },
+    methods: {
+        async applyDebit() {
+            const account = true//accountContext.getAccount();
+            if (!account) return;
+            const success = true//await debitUsecase.execute(account.accountNumber!, debit);
+            if (success) this.$router.push(`/home`);
+        }
+    }
+})
 </script>
 
 <template>
-    <div></div>
+    <div>
+        <h2>debit</h2>
+        <form @submit.prevent="applyDebit()">
+            <fieldset>
+                <div>
+                    <label htmlFor="value">value</label>
+                    <input type="number" id="value" v-model="debit.value" min={0.01} step={0.01} required="true" />
+                </div>
+            </fieldset>
+            <div>
+                <button type="submit">
+                    apply
+                </button>
+            </div>
+        </form>
+    </div>
 </template>
